17ac54b06b07819150147e621f3c4e8d1d87504a
Jan Redmann <janredmann1@gmail.com>
2021-10-29 13:59:34 +0200

    add game

--- P5-Advanced/js/game.js
+++ P5-Advanced/js/game.js
@@ -1,3 +1,52 @@
 class Game {
 
+	setup() {
+		this.player = new Player()
+		this.background = new Background()
+		this.obstacles = []
+	}
+
+	constructor() {
+		this.backgroundImages
+		this.coinImage
+	}
+
+	preload() {
+		this.backgroundImages = [
+			{ src: loadImage('assets/background/plx-1.png'), x: 0, speed: 0 },
+			{ src: loadImage('assets/background/plx-2.png'), x: 0, speed: 1 },
+			{ src: loadImage('assets/background/plx-3.png'), x: 0, speed: 2 },
+			{ src: loadImage('assets/background/plx-4.png'), x: 0, speed: 3 },
+			{ src: loadImage('assets/background/plx-5.png'), x: 0, speed: 4 }
+		]
+		this.playerImage = loadImage('assets/player/bb8.gif')
+		this.coinImage = loadImage('assets/coins/tile000.png')
+	}
+
+	draw() {
+		// console.log('game drawing')
+		clear()
+		this.background.draw()
+		this.player.draw()
+		// here we add coins to the obstacles array
+		// frameCount - this is provided by p5
+		if (frameCount % 10 === 0) {
+			this.obstacles.push(new Obstacle(this.coinImage))
+			console.log(this.obstacles)
+		}
+		// iterate over the obstacles array and call the draw function 
+		// for every obstacle inside
+
+		this.obstacles.forEach(function (obstacle) {
+			obstacle.draw()
+		})
+
+		this.obstacles = this.obstacles.filter(obstacle => {
+			if (obstacle.collision(this.player) || obstacle.x < 0 - obstacle.width) {
+				return false
+			} else {
+				return true
+			}
+		})
+	}
 }
